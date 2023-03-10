1d5628ca25f316d8d766ca798d8829d1fae08c11
Kwamena <83978246+Jake186@users.noreply.github.com>
2023-03-10 00:09:58 +0000

    api_and_web-stack
    web-stack

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Fixes Wordpress Site Error
+
+exec { 'fix-wordpress':
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+/var/www/html/wp-settings.php; service apache2 restart"',
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
